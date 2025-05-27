f1f61884b55bde28fbbdfbb0442b110db2c1f767
Jan Leirvaag <lja022025@gmail.com>
2025-05-27 21:41:44 +0200

    update

--- MainForm.cs
+++ MainForm.cs
@@ -11,13 +11,15 @@ public partial class Form1 : Form
         tbInput = new TextBox();
         tbInput.Location = new System.Drawing.Point(0, 100);
         tbInput.ReadOnly = false;
-        tbInput.Text = "Input box";
+        tbInput.Text = "What now?";
 
         //Hook up event
         tbInput.KeyDown += tbInput_KeyDown;
 
         rtbStoryBox = new RichTextBox();
         rtbStoryBox.Dock = DockStyle.Top;
+        rtbStoryBox.ForeColor = Color.Black;
+        rtbStoryBox.BackColor = Color.LightSalmon;
         rtbStoryBox.ReadOnly = true;
         rtbStoryBox.Text = "Just a test";
 
@@ -33,7 +35,23 @@ public partial class Form1 : Form
 
             rtbStoryBox.Text = Program.UserAction(userInput);
             tbInput.Clear();
+            tbInput.Text = "What now?";
+            tbInput.SelectAll();
         }
+    }
+
+    private void Init_BackGround()
+    {
+        //Set the application path
+        string projectRoot = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
+        string fileDataDir = "gfx";
+        string fileName = "forest2.jpg";
+        //Combine application path with where the csv file is (\Data\scrubbed.csv)
+        //Should eliminate hardcoding of file path as long as it's in the \Data dir with the compiled .exe a level above
+        string filePath = Path.Combine(projectRoot, fileDataDir, fileName);
+        Image myImage = new Bitmap(filePath);
+
+        this.BackgroundImage = myImage;
     }
 
     public Form1()
@@ -45,6 +63,7 @@ public partial class Form1 : Form
         this.Size = new System.Drawing.Size(500, 500);
         this.StartPosition = FormStartPosition.CenterScreen;
 
+        Init_BackGround();
         Initialize();
     }
 }
